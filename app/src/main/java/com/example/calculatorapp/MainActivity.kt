feccb7c98a1c66eb6e53015f40e8aa27bec85511
Bhagyesh Patil <bhagyeshp013@gmail.com>
2025-07-03 18:41:38 +0530

    feat: add splash screen on app launch

--- app/src/main/java/com/example/calculatorapp/MainActivity.kt
+++ app/src/main/java/com/example/calculatorapp/MainActivity.kt
@@ -3,11 +3,14 @@ package com.example.calculatorapp
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
@@ -15,6 +18,15 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        splashScreen.setOnExitAnimationListener { splashProvider ->
+            splashProvider.view.animate()
+                .alpha(0f)
+                .setDuration(400)
+                .withEndAction {
+                    splashProvider.remove()
+                }.start()
         }
     }
 }
